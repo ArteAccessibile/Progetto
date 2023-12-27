cdcf1b94410e818b672296d4fe040f4b0ec9371b
Francesco Giacomuzzo <giacomuzzofrancesco3@gmail.com>
2023-12-27 17:51:48 +0100

    create php files, update DBAccess

--- Codice/php/DBAccess.php
+++ Codice/php/DBAccess.php
@@ -1,5 +1,4 @@
 <?php
-
 class DBAccess {
     private $connection;
 
@@ -11,25 +10,30 @@ class DBAccess {
         }
     }
 
-    public function getFiles() {
-        $query = "SELECT * FROM files";
+    public function getOperas() {
+        $query = "SELECT titolo FROM opera"; // Selecting name of the opera from the 'opera' table
         $result = $this->connection->query($query);
-
-        $files = array();
-
+    
+        $operas = array();
+    
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
-                $files[] = $row;
+                $name = $row['titolo'];
+                $file_path = "immagini/" . str_replace(" ", "_", strtolower($name)) . ".jpg"; // Generating the file path based on the opera's name
+                $operas[] = array('titolo' => $name, 'file_path' => $file_path); // Adding each opera's name and file path to the $operas array
             }
         }
-
-        return $files;
+    
+        return $operas;
     }
+    
 
     public function closeConnection() {
         $this->connection->close();
     }
 }
+
+
 
 
 ?>
