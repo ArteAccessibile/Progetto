386bec05398a5d71224ccb924e58bbb59e8d46f0
Francesco Giacomuzzo <giacomuzzofrancesco3@gmail.com>
2023-12-27 18:44:40 +0100

    restore qcdb

--- DataBase/queryCreazioneDB.sql
+++ DataBase/queryCreazioneDB.sql
@@ -1,63 +1,32 @@
-
-USE fgiacomutest; --cambiare con il nome del database 
-
-DROP TABLE if EXISTS utente;
-
-DROP TABLE if EXISTS opera;
-DROP TABLE if EXISTS preferito;
-DROP TABLE if EXISTS artista;
-
--- Crea la tabella utente
 CREATE TABLE utente (
     email varchar(40) PRIMARY KEY,
     psw char(64) NOT NULL,
     nome varchar(20) NOT NULL,
     cognome varchar(20) NOT NULL,
     data_nascita date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
--- Inserisci valori nella tabella utente
-INSERT INTO utente (email, psw, nome, cognome, data_nascita) VALUES
-('email1@example.com', 'password1', 'Nome1', 'Cognome1', '1990-01-01'),
-('email2@example.com', 'password2', 'Nome2', 'Cognome2', '1995-02-02');
-
--- Crea la tabella artista
 CREATE TABLE artista (
     utente varchar(40) PRIMARY KEY REFERENCES utente(email),
-    descrizione TEXT NOT NULL,
+    descrizione varchar NOT NULL,
     pseudonimo varchar(30),
     email_contatto varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
--- Inserisci valori nella tabella artista
-INSERT INTO artista (utente, descrizione, pseudonimo, email_contatto) VALUES
-('email1@example.com', 'Descrizione artista 1', 'Pseudonimo1', 'email_artista1@example.com'),
-('email2@example.com', 'Descrizione artista 2', 'Pseudonimo2', 'email_artista2@example.com');
-
--- Crea la tabella opera
 CREATE TABLE opera (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    artista varchar(40) NOT NULL,
+    id serial PRIMARY KEY,
+    artista varchar(40) REFERENCES artista(utente) NOT NULL,
     titolo varchar(50) NOT NULL,
     desc_abbrev varchar(200) NOT NULL,
-    descrizione TEXT NOT NULL,
-    data_creazione date NOT NULL,
-    FOREIGN KEY (artista) REFERENCES artista(utente)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    descrizione varchar NOT NULL,
+    data_creazione date NOT NULL
+);
 
--- Inserisci valori nella tabella opera
-INSERT INTO opera (artista, titolo, desc_abbrev, descrizione, data_creazione) VALUES
-('email1@example.com', 'gattobaguette ', 'Descrizione abbreviata opera 1', 'Descrizione opera 1', '2023-01-01'),
-('email2@example.com', 'gattomirtillo', 'Descrizione abbreviata opera 2', 'Descrizione opera 2', '2023-02-02');
-
--- Crea la tabella preferito
 CREATE TABLE preferito (
     utente varchar (40) REFERENCES utente(email),
     opera BIGINT REFERENCES opera(id),
     PRIMARY KEY (utente, opera)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
--- Inserisci valori nella tabella preferito
-INSERT INTO preferito (utente, opera) VALUES
-('email1@example.com', 1),
-('email2@example.com', 2);
+ALTER TABLE opera ADD UNIQUE (titolo, artista);
+
