a9f17fcc63832eb9c900be8ebb37715455b6b021
Alberto <alberto.rizzo.dev@gmail.com>
2024-01-08 09:20:09 +0100

    modifiche sezione artista

---
+++ Codice/js/bottonenuovaopera.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('btnnewopera');
+
+
+function goToUpload() {
+    window.location.href = "../html/opera_upload.html";
+}
+
+btn.addEventListener('click', goToUpload);
