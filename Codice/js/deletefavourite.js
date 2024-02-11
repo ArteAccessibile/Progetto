09cc9d1e744a21c7b555b3673138082be91e01a1
Alberto <alberto.rizzo.dev@gmail.com>
2024-02-11 20:01:33 +0100

    fix js

--- Codice/js/deletefavourite.js
+++ Codice/js/deletefavourite.js
@@ -1,14 +1,19 @@
 
-document.getElementById('remove-favourite-button').addEventListener('click', function(event) {
-    // previene l'invio del modulo
-    event.preventDefault();
-  
-    // mostra un messaggio di conferma
-    var confirmDelete = window.confirm("Sei sicuro di voler rimuovere il preferito? Questa azione non può essere annullata.");
-  
-    // Se l'utente conferma, invia il modulo
-    if (confirmDelete) {
-      this.form.submit();
-    }
-  });
+const collection = document.getElementsByClassName("remove-favourite-button");
+
+Array.prototype.forEach.call(collection, function(button) {
+    button.addEventListener('click', function(event) {
+        // previene l'invio del modulo
+        event.preventDefault();
+      
+        // mostra un messaggio di conferma
+        var confirmDelete = window.confirm("Sei sicuro di voler rimuovere il preferito? Questa azione non può essere annullata.");
+      
+        // Se l'utente conferma, invia il modulo
+        if (confirmDelete) {
+          this.form.submit();
+        }
+      }
+    );
+});
   
